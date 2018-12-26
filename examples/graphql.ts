ecf2d21ac69e85ffc8ea4bf0b2d0f16b89cdc706
Vitor Luiz Cavalcanti <vitorluizc@outlook.com>
2018-12-26 18:50:24 -0200

    :white_check_mark: include GraphQLRequest module to examples

---
+++ examples/graphql.ts
@@ -0,0 +1,41 @@
+import createRequest from '../';
+
+type GraphQLResponse = {
+  data?: null | { [graph: string]: any; };
+  errors?: {
+    message?: string;
+  }[];
+};
+
+const resolveMessage = (error: { message?: string; }) => {
+  return (error && error.message) || 'Unknown error.';
+};
+
+export default createRequest(window.fetch, {
+  onRequest: (query: string, variables: { [name: string]: any; } = {}) => ({
+    url: 'https://api.github.com/graphql',
+    body: JSON.stringify({ query, variables }),
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  }),
+  onResponse: async (response) => {
+    try {
+      const { data, errors = [] } = await response.json() as GraphQLResponse;
+      if (data === null && errors.length > 0)
+        throw new Error(resolveMessage(errors[0]));
+      return data;
+    } catch (error) {
+      throw error;
+    }
+  },
+  onError: (error) => {
+    const message = resolveMessage(error);
+    console.dir({
+      ms: Date.now(),
+      message
+    });
+    return Promise.reject(message);
+  }
+});
