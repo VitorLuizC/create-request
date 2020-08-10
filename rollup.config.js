f36b1e5f6353f2fc540813e88c293297ad5dcae4
Vitor Luiz Cavalcanti <vitorluizc@outlook.com>
2020-08-09 23:43:54 -0300

    🏗 use same bundle strategy as other bitty packages

---
+++ rollup.config.js
@@ -0,0 +1,43 @@
+import { terser } from 'rollup-plugin-terser';
+
+/**
+ * Creates an output options object.
+ * @param {import('rollup').OutputOptions} options
+ * @returns {import('rollup').OutputOptions}
+ */
+const Option = (options) => ({
+  exports: 'named',
+  sourcemap: true,
+  ...options,
+});
+
+/**
+ * An object with all configuration for `Rollup.js`.
+ * @type {import('rollup').RollupOptions}
+ */
+const options = {
+  input: './src/index.js',
+  output: [
+    Option({
+      file: './dist/index.js',
+      format: 'commonjs',
+    }),
+    Option({
+      file: './dist/index.esm.js',
+      format: 'esm',
+    }),
+    Option({
+      file: './dist/index.umd.js',
+      name: 'createRequest',
+      format: 'umd',
+    }),
+    Option({
+      file: './dist/index.umd.min.js',
+      name: 'createRequest',
+      format: 'umd',
+      plugins: [terser()],
+    }),
+  ],
+};
+
+export default options;
